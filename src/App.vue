ca2ad97599b7590e122565753a495755b69023e4
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-23 03:24:54 +0800

    012 Adding a Component

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <friend-contact></friend-contact>
+        <friend-contact></friend-contact>
+    </section>
+</template>
+<script>
+
+
+export default{
+    data(){
+        return {
+            friends:[
+                {
+                    id:'Manu',
+                    name: 'Manuel Lorenz',
+                    phone: '09088952681',
+                    email: 'manuellorenz@gmail.com',
+                },
+                {
+                    id:'Jules',
+                    name: 'Julia Jones',
+                    phone: '09277779277',
+                    email: 'juliajones@gmail.com',
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,30 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetail">{{isDetailVisible === true ? 'Hide Details' : 'Toggle Details'}}</button>
+    <ul v-if="isDetailVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong>{{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      friend: {
+        id: "Manu",
+        name: "Manuel Lorenz",
+        phone: "09088952681",
+        email: "manuellorenz@gmail.com",
+      },
+      isDetailVisible: false
+    };
+  },
+  methods:{
+    toggleDetail(){
+        this.isDetailVisible = !this.isDetailVisible;
+    }
+  }
+};
+</script>
