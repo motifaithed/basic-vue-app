b6231d7cd17f0e0c416de7e074d420fe8dd0cf01
Ronel Andaya <ronel.andaya@gmail.com>
2022-08-25 09:29:07 +0800

    008 Defining & Validating Custom Events

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,8 +1,12 @@
 <template>
   <li>
-    <h2>{{ name }} {{ isFavorite == true ? '(Favorite)': '' }}</h2>
-    <button @click="toggleDetail">{{isDetailVisible === true ? 'Hide' : 'Show'}}  Details</button>
-    <button @click="toggleFavorite">{{isFavorite === true ? 'Remove as' : 'Set as'}}  Favorite</button>
+    <h2>{{ name }} {{ isFavorite == true ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleDetail">
+      {{ isDetailVisible === true ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavorite">
+      {{ isFavorite === true ? "Remove as" : "Set as" }} Favorite
+    </button>
     <ul v-if="isDetailVisible">
       <li><strong>Phone:</strong>{{ phone }}</li>
       <li><strong>Email:</strong>{{ email }}</li>
@@ -11,22 +15,26 @@
 </template>
 <script>
 export default {
-    props:[
-        'id','name', 'phone','email', 'isFavorite'
-    ],
+  props: {
+    id: String,
+    name: String,
+    phone: String,
+    email: String,
+    isFavorite: Boolean,
+  },
+  emits: ["toggle-favorite"],
   data() {
     return {
       isDetailVisible: false,
     };
   },
-  methods:{
-    toggleDetail(){
-        this.isDetailVisible = !this.isDetailVisible;
+  methods: {
+    toggleDetail() {
+      this.isDetailVisible = !this.isDetailVisible;
     },
-    toggleFavorite(){
-        this.$emit('toggle-favorite', this.id);
-    }
-  }
+    toggleFavorite() {
+      this.$emit("toggle-favorite", this.id);
+    },
+  },
 };
 </script>
-
